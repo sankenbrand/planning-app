2b5ab3111ccd131b573626c0bac240d11f66b55f
ANKENBRAND Sharine <sharine.ankenbrand@soprasteria.com>
2022-06-10 14:44:10 +0200

    add store for selected date

---
+++ src/stores/selectedDate.ts
@@ -0,0 +1,12 @@
+import { writable } from "svelte/store"
+
+// Get the value out of storage on load.
+const stored = localStorage.selectedDate
+// or localStorage.getItem('selectedDate')
+
+// Set the stored value or a sane default.
+export const selectedDate = writable(stored || "no date selected")
+
+// Anytime the store changes, update the local storage value.
+selectedDate.subscribe((value) => (localStorage.selectedDate = value))
+// or localStorage.setItem('selectedDate', value)
