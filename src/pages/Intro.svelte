3a417a151fd3a16e569ec6a1f5aab4cb1e158815
ANKENBRAND Sharine <sharine.ankenbrand@soprasteria.com>
2022-06-10 15:03:59 +0200

    add landing page

---
+++ src/pages/Intro.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { Link } from "svelte-navigator"
+  import Button from "../components/buttons/Button.svelte"
+  import ArrowRightSvg from "../components/svg/ArrowRightSVG.svelte"
+</script>
+
+<section class="card_layout">
+  <article class="card">
+    <article class="card_left_side">
+      <img
+        src="src/assets/illustrations/events.svg"
+        width="80%"
+        alt="Calendar Illustration"
+      />
+    </article>
+    <article>
+      <h1>Let's plan!</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ac dui
+        iaculis, condimentum sapien ac, interdum libero.
+      </p>
+      <Link to="/home" style="text-decoration: none">
+        <Button type="button" CTA_Name="Get started">
+          <ArrowRightSvg />
+        </Button>
+      </Link>
+    </article>
+  </article>
+</section>
