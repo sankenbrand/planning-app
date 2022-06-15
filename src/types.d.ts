bb379184dd7979011739c6b0b5e4851f69859429
ANKENBRAND Sharine <sharine.ankenbrand@soprasteria.com>
2022-06-15 16:29:10 +0200

    add global types

---
+++ src/types.d.ts
@@ -0,0 +1,22 @@
+interface Category {
+  categoryId: number
+  categoryName: string
+}
+
+interface User {
+  id: number
+  name: string
+  date: string
+  category: string
+  preferredTime: string
+  selectedTime: string | null
+  eventId?: number
+}
+
+interface Event {
+  id: number
+  date: string
+  category: string | null
+  time: string | null
+  users: User[]
+}
