bff18ba8cc1cb681239dcaa8a87c22fb4e0e9401
ANKENBRAND Sharine <sharine.ankenbrand@soprasteria.com>
2022-06-15 13:52:06 +0200

    add greeting message based on time

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  const greeting = () => {
+    var myDate = new Date()
+    var hours = myDate.getHours()
+    var greet
+
+    if (hours < 12) greet = "morning"
+    else if (hours >= 12 && hours <= 17) greet = "afternoon"
+    else if (hours >= 17 && hours <= 24) greet = "evening"
+
+    return greet
+  }
+</script>
+
+<h1>Good {greeting()}!</h1>
